272dacacb2db34c1c658bcb975264f4c86512ccc
Kundan <kundan kamble>
2022-05-27 22:03:41 -0700

    [KUNDAN] Added Find Max Integer program

---
+++ GenericProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericProblem
+
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FindMaxInteger maxintnum = new FindMaxInteger();
+            Console.WriteLine(maxintnum.NumberMax(10, 80, 10));
+        }
+    }
+}
