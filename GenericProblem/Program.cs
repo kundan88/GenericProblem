aeb7877b5d7a89dedf06fbef5a9267e31e185e3e
Kundan <kundan kamble>
2022-05-27 23:15:43 -0700

    [KUNDAN] Added Find Max String program

--- GenericProblem/Program.cs
+++ GenericProblem/Program.cs
@@ -12,6 +12,9 @@ namespace GenericProblem
             Console.WriteLine("-------------------------");
             FindMaxFloat maxfloatnum = new FindMaxFloat();
             Console.WriteLine(maxfloatnum.FloatNumber(90.4f, 50.6f, 70.7f));
+            Console.WriteLine("find Max String");
+            string output = FindMaxString.MaximumString("Hi", "This", "is");
+            Console.WriteLine(output);
         }
     }
 }
